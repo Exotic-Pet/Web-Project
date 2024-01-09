87c45665561ca2fdf6603e415539f8205eff0389
Meriser <smt5783@gmail.com>
2024-01-09 16:52:27 +0800

    2024/1/9 AutoScrollTop 元件建立、App 套用
    解決Router頁面置頂問題

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { Routes, Route } from "react-router-dom";
 import { BrowserRouter } from "react-router-dom";
 import "./styles/all.css";
+import AutoScrollTop from "./components/AutoScrollTop";
 import Layout from "./components/Layout";
 import HomePage from "./pages/HomePage";
 import StoreOverview from "./pages/StoreOverview";
@@ -10,14 +11,16 @@ import StoreContent from "./pages/StoreContent";
 export default function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/page" element={<Layout />} >
-          <Route path="store-overview" element={<StoreOverview />} />
-          <Route path="store-search" element={<StoreSearch />} />
-          <Route path="store-search/content" element={<StoreContent />} />
-        </Route>
-      </Routes>
+      <AutoScrollTop>
+        <Routes>
+          <Route index element={<HomePage />} />
+          <Route path="/page" element={<Layout />}>
+            <Route path="store-overview" element={<StoreOverview />} />
+            <Route path="store-search" element={<StoreSearch />} />
+            <Route path="store-search/content" element={<StoreContent />} />
+          </Route>
+        </Routes>
+      </AutoScrollTop>
     </BrowserRouter>
   );
 }
