4d8fe17c6de50dd998d60e2903e7c1fde3519921
Meriser <smt5783@gmail.com>
2024-01-09 16:00:27 +0800

    2024/1/9 Router 優化（解決 header 切換問題）

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
 import { Routes, Route } from "react-router-dom";
 import { BrowserRouter } from "react-router-dom";
 import "./styles/all.css";
-import HomeHeader from "./components/HomeHeader";
-import Header from "./components/Header";
-import Footer from "./components/Footer";
-import StoreOverview from "./pages/StoreOverview";
+import Layout from "./components/Layout";
 import HomePage from "./pages/HomePage";
+import StoreOverview from "./pages/StoreOverview";
 import StoreSearch from "./pages/StoreSearch";
 import StoreContent from "./pages/StoreContent";
 
@@ -13,16 +11,13 @@ export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<HomeHeader />}></Route>
-        <Route path="/page/*" element={<Header />}></Route>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/page" element={<Layout />} >
+          <Route path="store-overview" element={<StoreOverview />} />
+          <Route path="store-search" element={<StoreSearch />} />
+          <Route path="store-search/content" element={<StoreContent />} />
+        </Route>
       </Routes>
-      <Routes>
-        <Route path="/" element={<HomePage />}></Route>
-        <Route path="/page/store-overview" element={<StoreOverview />}></Route>
-        <Route path="/page/store-search" element={<StoreSearch />}></Route>
-        <Route path="/page/store-search/content" element={<StoreContent />}></Route>
-      </Routes>
-      <Footer />
     </BrowserRouter>
-  )
+  );
 }
