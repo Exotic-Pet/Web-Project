a534ea7665e945853a87f7b8b9a62bdd92e0fad0
Meriser <smt5783@gmail.com>
2024-02-01 22:40:09 +0800

    2024/2/1 SweetAlert2 安裝、SignIn 註冊登入開發
    1. SweetAlert2 相關套件安裝
    2. 使用 localStorage 存資料，運用 SweetAlert 呈現結果

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,12 +1,66 @@
 import { useState } from "react";
 import "../styles/all.css";
+import Swal from "sweetalert2";
 
 import googleIcon from "../assets/signIn/google.svg";
 import facebookIcon from "../assets/signIn/facebook.svg";
 import lineIcon from "../assets/signIn/line.svg";
 
 export default function SignIn({ isOpenState, setIsOpenState, toggleState }) {
+  // Toggle Page
   const [isSignIn, setIsSignIn] = useState(true);
+
+  // User Input Info
+  const [account, setAccount] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = () => {
+    if (isSignIn) {
+      const storedAccount = localStorage.getItem("account");
+      const storedPassword = localStorage.getItem("password");
+      if (account === storedAccount && password === storedPassword) {
+        // Login Success
+        Swal.fire({
+          title: " 登入成功！",
+          icon: "success",
+          confirmButtonText: "前往探索",
+          backdrop: true,
+        });
+        setIsOpenState(false);
+      } else {
+        // Login Error
+        Swal.fire({
+          title: "帳號或密碼錯誤",
+          icon: "error",
+          confirmButtonText: "重新嘗試",
+          backdrop: true,
+        });
+      }
+    } else {
+      // Register
+      if (account.trim() !== "" && password.trim() !== "") {
+        localStorage.setItem("account", account);
+        localStorage.setItem("password", password);
+        Swal.fire({
+          title: " 註冊成功！",
+          icon: "success",
+          confirmButtonText: "確定",
+          backdrop: true,
+        }).then(() => {
+          // Reset Input Text
+          setAccount("");
+          setPassword("");
+        });
+      } else {
+        Swal.fire({
+          title: "請正確輸入",
+          icon: "error",
+          confirmButtonText: "重新嘗試",
+          backdrop: true,
+        });
+      }
+    }
+  };
 
   return (
     <div
@@ -14,7 +68,7 @@ export default function SignIn({ isOpenState, setIsOpenState, toggleState }) {
       onClick={(e) => toggleState(e, isOpenState, setIsOpenState)}
     >
       <article
-        className="lightBox__content max-w-[90%] lg:max-w-[548px] max-h-[90vh] overflow-y-auto tracking-wide bg-white rounded-[20px] p-6 md:p-12"
+        className="lightBox__content lightBox__scrollbar lightBox__scrollbar--green max-w-[90%] lg:max-w-[548px] max-h-[90vh] overflow-y-auto tracking-wide bg-white rounded-[20px] p-6 md:p-12"
         onClick={(e) => e.stopPropagation()}
       >
         <div className="flex gap-x-3 px-6 py-[14px] bg-gray-dark rounded-full">
@@ -39,22 +93,29 @@ export default function SignIn({ isOpenState, setIsOpenState, toggleState }) {
           <label htmlFor="account" className="mb-6 block">
             <span className="text-[18px] font-bold mr-5">帳號</span>
             <input
-              className="px-6 py-[10px] mt-2 sm:mt-0 block sm:inline sm:min-w-[320px] border border-[1.5px] border-brown-normal rounded-full"
+              className="input__style--green outline  outline-[1.5px] outline-brown-normal px-6 py-[10px] mt-2 sm:mt-0 block sm:inline sm:min-w-[320px] rounded-full"
               type="email"
               id="account"
               placeholder={`請輸入${isSignIn ? "使用者名稱 / " : ""}Email`}
+              value={account}
+              onChange={(e) => setAccount(e.target.value)}
             />
           </label>
           <label htmlFor="password" className="mb-9 block">
             <span className="text-[18px] font-bold mr-5">密碼</span>
             <input
-              className="px-6 py-[10px] mt-2 sm:mt-0 block sm:inline sm:min-w-[320px] border border-[1.5px] border-brown-normal rounded-full focus:border-green-normal"
+              className="input__style--green outline  outline-[1.5px] outline-brown-normal px-6 py-[10px] mt-2 sm:mt-0 block sm:inline sm:min-w-[320px] rounded-full focus:border-green-normal"
               type="password"
               id="password"
               placeholder="請輸入密碼"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
             />
           </label>
-          <button className="bg-green-light block rounded-full w-full py-4 tracking-wider font-bold text-xl hover:text-white">
+          <button
+            className="bg-green-light block rounded-full w-full py-4 tracking-wider font-bold text-xl hover:text-white"
+            onClick={handleSubmit}
+          >
             {isSignIn ? "登 入" : "註冊會員"}
           </button>
           <div className="mt-6 flex justify-evenly">
