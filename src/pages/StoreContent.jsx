f6ee4b04208f5c1e76f1831c1f935172daab7463
Teddy Su <f0911297201@gmail.com>
2024-02-06 11:50:52 +0800

    2024/2/6 咖啡店內頁資訊圖片更換＋文字內容修改＋底部新增免責聲明
    咖啡店內頁資訊圖片更換
    文字內容修改
    底部新增免責聲明

--- src/pages/StoreContent.jsx
+++ src/pages/StoreContent.jsx
@@ -1,11 +1,22 @@
 import "../styles/all.css";
 import shareFacebook from '../assets/store/share-facebook.svg';
 import shareLine from '../assets/store/share-line.svg';
-import contentImg_1 from '../assets/store/content-img1.png';
-import contentImg_2 from '../assets/store/content-img2.png';
-import contentImg_3 from '../assets/store/content-img3.png';
-import contentImg_4 from '../assets/store/content-img4.png';
-import contentImg_5 from '../assets/store/content-img5.svg';
+import mainImg1 from '../assets/store/imageForContent/mainImg-1.png';
+import mainImg2 from '../assets/store/imageForContent/mainImg-2.png';
+import mainImg3 from '../assets/store/imageForContent/mainImg-3.png';
+import mainImg4 from '../assets/store/imageForContent/mainImg-4.png';
+import articleImg1 from '../assets/store/imageForContent/articleImg-1.png';
+import articleImg2 from '../assets/store/imageForContent/articleImg-2.png';
+import articleImg3 from '../assets/store/imageForContent/articleImg-3.png';
+import articleImg4 from '../assets/store/imageForContent/articleImg-4.png';
+import articleImg5 from '../assets/store/imageForContent/articleImg-5.png';
+import articleImg6 from '../assets/store/imageForContent/articleImg-6.png';
+import articleImg7 from '../assets/store/imageForContent/articleImg-7.png';
+import articleImg8 from '../assets/store/imageForContent/articleImg-8.png';
+import articleImg9 from '../assets/store/imageForContent/articleImg-9.png';
+import articleImg10 from '../assets/store/imageForContent/articleImg-10.png';
+import gdn1 from '../assets/store/imageForContent/GDN-1.png';
+import gdn2 from '../assets/store/imageForContent/GDN-2.png';
 import facebook from '../assets/store/facebook.svg';
 import telephone from '../assets/store/telephone.svg';
 import location from '../assets/store/location.svg';
@@ -15,8 +26,9 @@ import time from '../assets/store/time.svg';
 export default function StoreContent() {
     return (
     // Store Content
-    <main className="mt-[84px] container mx-auto overflow-hidden md:flex">
-        <article className="mt-8 px-2 lg:px-0">
+    <>
+        <main className="mt-[84px] container mx-auto overflow-hidden md:flex">
+            <article className="mt-8 px-2 lg:px-0 max-w-[859px]">
             <h4 className="flex items-center text-base font-bold text-white mb-[13px]">
                 <div className="w-[117px] h-[29px] bg-[url('../assets/store/label.svg')] bg-no-repeat flex justify-center items-center">
                     <strong className="pr-2 block">
@@ -31,16 +43,16 @@ export default function StoreContent() {
                 <a href="#"><img src={shareFacebook} alt="facebook分享" className="mx-[11px]" /></a>
                 <a href="#"><img src={shareLine} alt="line分享" /></a>
             </div>
-            <p className="text-base font-bold text-brown-dark">【Pythonimd玩蟒人生】是一間以球蟒與爬蟲類為主題的咖啡館，位於大同區捷運大橋頭站的【玩蟒人生】低消只要一杯飲料就能與各種球蟒或是爬蟲類近距離互動，老闆本身專業繁殖球蟒，有任何關於爬蟲類的知識都能詢問，老闆會給予耐心又專業的解答，超酷的台北另類寵物咖啡館。</p>
-            <img src={contentImg_1} alt="content_img" className="my-[35px]" />
-            <img src={contentImg_2} alt="content_img" className="mb-[35px]" />
-            <p className="text-base font-bold text-brown-dark">有一整面牆擺放了滿滿的透明飼養箱，一區上面有數字編號、一區是沒有編號的，每個箱子裡都住了一條蟒蛇，箱子上有數字的是可以互動的蟒蛇，沒有數字的則是店內販售展示的蟒蛇。</p>
-            <p className="text-base font-bold text-brown-dark mb-[18px]">【Pythonimd玩蟒人生】主要提供飲料，像是咖啡、果汁、碳酸飲料等，另外有少許的蛋糕，價格大約落在$130至$200左右，這裡的低消就是每人一杯飲料，一杯飲料就能和蛇蛇互動個人覺得很值得。 </p>
-            <img src={contentImg_3} alt="content_img" className="my-[35px]" />
-            <img src={contentImg_4} alt="content_img" className="mb-[35px]" />
-            <p className="text-base font-bold text-brown-dark">球蟒與爬蟲類主題咖啡廳實在很特別，能近距離與爬小孩互動，對爬小孩有任何問題都能詢問老闆，也給小孩飼養動物、接觸動物的正確觀念。
-                如果你也喜歡蟒蛇與爬蟲動物，來【玩蟒人生Pythonism】只要一杯飲料就能實現與爬小孩親密接觸的夢想，推薦給大家囉。</p>
-            <div className="flex flex-col mt-12 mb-24 pt-10 border-t-[20px] border-cyan-normal xl:flex-row">
+            <p className="text-base font-bold text-brown-dark">【Pythonism玩蟒人生】是一家以球蟒和爬蟲為主題的咖啡館，在捷運大橋頭站附近。這裡的低消僅需一杯飲料，就能與各種球蟒或爬蟲類互動。老闆本身是專業的球蟒繁殖者，對於爬蟲類的知識有豐富的瞭解，客人可以隨時向他諮詢！【Pythonism玩蟒人生】是間非常特別的咖啡廳～</p>
+            <img src={mainImg1} alt="content_img" className="my-[35px]" />
+            <img src={mainImg2} alt="content_img" className="mb-[35px]" />
+            <p className="text-base font-bold text-brown-dark mb-[18px]">店內的櫃子擺放了很多透明壓克力箱，每個箱子裡都住了一條蛇，上面都有編號，箱子上有數字的是可以互動的蟒蛇，老闆會告訴你可以互動的蛇的編號，而沒有數字的則是店內販售展示的蟒蛇。</p>
+            <p className="text-base font-bold text-brown-dark mb-[18px]">【Pythonism玩蟒人生】主要供應各種飲料，包括咖啡、果汁、碳酸飲料等，還有一些精緻的蛋糕。價格大致在$130至$200之間，而低消則是每人一杯飲料。在這裡，一杯飲料的價格就能與蛇蛇有親密的互動體驗，相當超值。 </p>
+            <img src={mainImg3} alt="content_img" className="my-[35px]" />
+            <img src={mainImg4} alt="content_img" className="mb-[35px]" />
+            <p className="text-base font-bold text-brown-dark">這家球蟒和爬蟲類主題的咖啡廳真的很特別，讓你能與這些可愛的小生物近距離互動。如果有任何關於爬蟲類的問題，都可以向老闆詢問，他非常樂意提供正確的知識。這不僅為大小朋友提供了飼養動物、接觸動物的機會，也有助於培養他們正確的動物觀念。
+                <br/>如果你也喜歡蟒蛇和爬蟲動物，來【玩蟒人生Pythonism】只需點一杯飲料，就能跟他們親密接觸。極力推薦給大家！</p>
+            <div className="flex flex-col mt-12 mb-20 pt-10 border-t-[20px] border-cyan-normal xl:flex-row">
                 <div className="mx-auto min-w-[374px] sm:w-[432px] h-[275px] xl:mr-[60px]">
                     <iframe className="w-full h-full" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3614.716655041767!2d121.52333804856978!3d25.043688092922853!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3442a90a06f2c70d%3A0x1daef20b13b189f!2z54it6a6uUExVUy3lloTlsI7lr7rlupc!5e0!3m2!1szh-TW!2stw!4v1703740331398!5m2!1szh-TW!2stw"></iframe>
                 </div>
@@ -66,12 +78,12 @@ export default function StoreContent() {
                     </strong>
                 </div>
             </div>
-        </article>
-        <aside className="mt-10 pb-24 px-2 md:w-[300px] lg:px-0 lg:ml-6 xl:ml-[113px]">
+            </article>
+            <aside className="mt-10 pb-24 px-2 md:w-[300px] lg:px-0 lg:ml-6 xl:ml-[113px]">
             <h3 className="tracking-wide pb-5 mb-5 text-xl font-bold border-b-2 border-solid text-cyan-dark border-cyan-dark">相關文章</h3>
             <ul>
                 <li className="flex items-center mb-6">
-                    <img src={contentImg_5} alt="content" className="mr-3" />
+                    <img src={articleImg1} alt="content" className="mr-3" />
                     <div>
                         <h4 className="text-[15px] font-bold text-brown-dark">台北肉球森林和可愛的貓咪、大嘴鳥、狐獴互動</h4>
                         <div className="flex">
@@ -82,7 +94,7 @@ export default function StoreContent() {
                     </div>
                 </li>
                 <li className="flex items-center mb-6">
-                    <img src={contentImg_5} alt="content" className="mr-3" />
+                    <img src={articleImg2} alt="content" className="mr-3" />
                     <div>
                         <h4 className="text-[15px] font-bold text-brown-dark">台北肉球森林和可愛的貓咪、大嘴鳥、狐獴互動</h4>
                         <div className="flex">
@@ -93,7 +105,7 @@ export default function StoreContent() {
                     </div>
                 </li>
                 <li className="flex items-center mb-6">
-                    <img src={contentImg_5} alt="content" className="mr-3" />
+                    <img src={articleImg3} alt="content" className="mr-3" />
                     <div>
                         <h4 className="text-[15px] font-bold text-brown-dark">台北肉球森林和可愛的貓咪、大嘴鳥、狐獴互動</h4>
                         <div className="flex">
@@ -104,7 +116,7 @@ export default function StoreContent() {
                     </div>
                 </li>
                 <li className="flex items-center mb-6">
-                    <img src={contentImg_5} alt="content" className="mr-3" />
+                    <img src={articleImg4} alt="content" className="mr-3" />
                     <div>
                         <h4 className="text-[15px] font-bold text-brown-dark">台北肉球森林和可愛的貓咪、大嘴鳥、狐獴互動</h4>
                         <div className="flex">
@@ -115,7 +127,7 @@ export default function StoreContent() {
                     </div>
                 </li>
                 <li className="flex items-center mb-6">
-                    <img src={contentImg_5} alt="content" className="mr-3" />
+                    <img src={articleImg5} alt="content" className="mr-3" />
                     <div>
                         <h4 className="text-[15px] font-bold text-brown-dark">台北肉球森林和可愛的貓咪、大嘴鳥、狐獴互動</h4>
                         <div className="flex">
@@ -129,7 +141,7 @@ export default function StoreContent() {
             <h3 className="tracking-wide pb-5 mb-5 text-xl font-bold border-b-2 border-solid text-cyan-dark border-cyan-dark">熱門文章</h3>
             <ul>
                 <li className="flex items-center mb-6">
-                    <img src={contentImg_5} alt="content" className="mr-3" />
+                    <img src={articleImg6} alt="content" className="mr-3" />
                     <div>
                         <h4 className="text-[15px] font-bold text-brown-dark">台北肉球森林和可愛的貓咪、大嘴鳥、狐獴互動</h4>
                         <div className="flex">
@@ -140,7 +152,7 @@ export default function StoreContent() {
                     </div>
                 </li>
                 <li className="flex items-center mb-6">
-                    <img src={contentImg_5} alt="content" className="mr-3" />
+                    <img src={articleImg7} alt="content" className="mr-3" />
                     <div>
                         <h4 className="text-[15px] font-bold text-brown-dark">台北肉球森林和可愛的貓咪、大嘴鳥、狐獴互動</h4>
                         <div className="flex">
@@ -151,7 +163,7 @@ export default function StoreContent() {
                     </div>
                 </li>
                 <li className="flex items-center mb-6">
-                    <img src={contentImg_5} alt="content" className="mr-3" />
+                    <img src={articleImg8} alt="content" className="mr-3" />
                     <div>
                         <h4 className="text-[15px] font-bold text-brown-dark">台北肉球森林和可愛的貓咪、大嘴鳥、狐獴互動</h4>
                         <div className="flex">
@@ -162,7 +174,7 @@ export default function StoreContent() {
                     </div>
                 </li>
                 <li className="flex items-center mb-6">
-                    <img src={contentImg_5} alt="content" className="mr-3" />
+                    <img src={articleImg9} alt="content" className="mr-3" />
                     <div>
                         <h4 className="text-[15px] font-bold text-brown-dark">台北肉球森林和可愛的貓咪、大嘴鳥、狐獴互動</h4>
                         <div className="flex">
@@ -173,7 +185,7 @@ export default function StoreContent() {
                     </div>
                 </li>
                 <li className="flex items-center mb-6">
-                    <img src={contentImg_5} alt="content" className="mr-3" />
+                    <img src={articleImg10} alt="content" className="mr-3" />
                     <div>
                         <h4 className="text-[15px] font-bold text-brown-dark">台北肉球森林和可愛的貓咪、大嘴鳥、狐獴互動</h4>
                         <div className="flex">
@@ -184,9 +196,15 @@ export default function StoreContent() {
                     </div>
                 </li>
             </ul>
-            <div className="mx-auto mt-[75px] mb-[39px] w-[300px] h-[200px] leading-[200px] font-bold text-xl bg-gray-300 text-center">GDN</div>
-            <div className="mx-auto text-center w-[300px] h-[200px] leading-[200px] justify-center font-bold text-xl bg-gray-300">GDN</div>
-        </aside>
-    </main>
+            <div className="mx-auto mt-[75px] mb-[39px]"><img src={gdn1} alt="GDN" className="mx-auto"/></div>
+            <div className="mx-auto"><img src={gdn2} alt="GDN" className="mx-auto"/></div>
+            </aside>
+        </main>
+        <div className=" bg-gray-normal border border-brown-light rounded-[47px] py-[15px] mb-[90px] px-10 sm:px-28 max-w-[867px] sm:mx-20">
+                <h4 className="text-brown-dark font-bold text-sm mb-[6px]">照片來源：玩蟒人生 Pythonism FB 粉絲專頁</h4>
+                <p className="text-brown-dark font-medium text-sm mb-1">若您認為有侵權情形，煩請立即告知，我們將移除相關內容並下架，以確保維護相關權益。</p>
+                <p className="text-brown-dark font-medium text-sm">我們一向重視知識產權和合法使用，致力於建立一個公正、尊重著作權的環境，感謝您的合作與理解。</p>
+        </div>
+    </>
     )
 }
