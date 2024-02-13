30b8ed22bbddd8e167d8976c453c50db97158c97
Meriser <smt5783@gmail.com>
2024-02-13 15:49:59 +0800

    2024/2/13 HomePage 篩選功能製作、細節優化
    1. News "店家"、"活動" 資料篩選
    2. News 調整 hover 範圍、新增 Link to 連結
    3. 熱門文章區塊 2xl 新增背景色
    4. AOS 效果微調

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,11 +1,12 @@
 import { Link } from "react-router-dom";
+import { useState } from "react";
 // Components
 import AosInit from "../components/AosInit";
 import HomeHeader from "../components/HomeHeader";
 import Footer from "../components/Footer";
 // Images
 import arrowRight from "../assets/index/arrow-right.svg";
-import newsImg_1 from "../assets/index/news-1.png";
+
 import newsEvent_1 from "../assets/index/news-event-1.png";
 import newsEvent_2 from "../assets/index/news-event-2.png";
 import newsEvent_3 from "../assets/index/news-event-3.png";
@@ -84,61 +85,115 @@ export default function HomePage() {
   ];
 
   const newsData = [
-  {
-    id: 1,
-    year: 2024,
-    date: "01.22",
-    type: "活動",
-    imgPath: newsEvent_1,
-    title: "全台最大的爬蟲博覽會，即將在花博登場！",
-    content: "親愛的爬蟲愛好者們，準備好了嗎？今年全台最大的爬蟲博覽會即將在花博登場！這將是一場爬蟲世界的盛宴，將展示各種令人驚嘆的爬蟲類動物，以及相關的設備、用品和知識。無論你是對爬蟲類動物充滿好奇心，還是已經是一位經驗豐富的飼主，這個博覽會都將是你不容錯過的活動！"
-  },
-  {
-    id: 2,
-    year: 2024,
-    date: "02.01",
-    type: "活動",
-    imgPath: newsEvent_2,
-    title: "零距離觸摸爬蟲類動物，大小朋友都可參加！親子活動推薦",
-    content: "一般人對爬蟲類動物往往感到恐懼，並且會避之唯恐不及。但許多家長或許已經發現，小朋友卻對這些生物感到非常感興趣，並且毫不畏懼。事實上，有許多團體舉辦各種爬蟲類動物的探索活動，這些活動讓小朋友能夠從實際體驗中認識和了解這些冷門動物的生態習性，並超越了書本上的知識。"
-  },
-  {
-    id: 3,
-    year: 2024,
-    date: "02.13",
-    type: "活動",
-    imgPath: newsEvent_3,
-    title: "高雄夢時代 - 鼠兔趣味競賽，歡迎飼主報名參加！",
-    content: "這場競賽將在高雄夢時代舉辦，是一個讓你和你的寵物一同參與的盛大活動。無論是你的兔子還是你的寵物鼠，都可以在這裡展現自己的才能，一展風采！比賽將包括各種有趣的趣味競賽項目，例如快樂小跑、障礙賽、食物競速等等。這些競賽既能展現你的寵物的活力和技巧，也能讓你們之間建立更加緊密的關係。"
-  },
-  {
-    id: 4,
-    year: 2024,
-    date: "02.20",
-    type: "店家",
-    imgPath: newsStore_1,
-    title: "另類寵物主題餐廳～10間不可錯過的特寵主題餐廳",
-    content: "這篇收錄的不是一般的貓咪或狗狗寵物餐廳，而是以狐獴、巨嘴鳥、兔子、守宮、球蟒、鸚鵡、鬆獅蜥等為主題的另類特寵主題餐廳。這份精選介紹了10間獨特的寵物主題餐廳，快來親密互動，與這些可愛的萌寵們共度美好時光吧。"
-  },
-  {
-    id: 5,
-    year: 2024,
-    date: "02.19",
-    type: "店家",
-    imgPath: newsStore_2,
-    title: "動物嘉年華餐廳，天竺鼠、貓咪、兔子同樂的歡樂饗宴！",
-    content: "這裡是兔子和天竺鼠們的冒險樂園。牠們在這裡玩耍、跳躍，總是充滿好奇心的樣子。您可以和兔子們互動，一同參與這場充滿歡樂的冒險，當然最吸引人的還是美味的饗宴。餐廳提供各種豐富的料理，滿足您的味蕾，同時，您可以在歡樂的氛圍中享受美食。"
-  },
-  {
-    id: 6,
-    year: 2024,
-    date: "02.17",
-    type: "店家",
-    imgPath: newsStore_3,
-    title: "與神秘的爬蟲類共度下午茶時光～還可跟牠們互動哦～",
-    content: "在這裡，與爬蟲們的互動是不可或缺的環節。餐廳提供了專業的爬蟲導覽員，他們會教你如何與這些生物進行親密接觸，讓你不僅能看到牠們，還能親手摸摸，甚至學習更多有趣的爬蟲知識。"
-  },
-];
+    {
+      id: 1,
+      year: 2024,
+      date: "01.22",
+      type: "活動",
+      imgPath: newsEvent_1,
+      title: "全台最大的爬蟲博覽會，即將在花博登場！",
+      content:
+        "親愛的爬蟲愛好者們，準備好了嗎？今年全台最大的爬蟲博覽會即將在花博登場！這將是一場爬蟲世界的盛宴，將展示各種令人驚嘆的爬蟲類動物，以及相關的設備、用品和知識。無論你是對爬蟲類動物充滿好奇心，還是已經是一位經驗豐富的飼主，這個博覽會都將是你不容錯過的活動！",
+    },
+    {
+      id: 2,
+      year: 2024,
+      date: "02.01",
+      type: "活動",
+      imgPath: newsEvent_2,
+      title: "零距離觸摸爬蟲類動物，大小朋友都可參加！親子活動推薦",
+      content:
+        "一般人對爬蟲類動物往往感到恐懼，並且會避之唯恐不及。但許多家長或許已經發現，小朋友卻對這些生物感到非常感興趣，並且毫不畏懼。事實上，有許多團體舉辦各種爬蟲類動物的探索活動，這些活動讓小朋友能夠從實際體驗中認識和了解這些冷門動物的生態習性，並超越了書本上的知識。",
+    },
+    {
+      id: 3,
+      year: 2024,
+      date: "02.13",
+      type: "活動",
+      imgPath: newsEvent_3,
+      title: "高雄夢時代 - 鼠兔趣味競賽，歡迎飼主報名參加！",
+      content:
+        "這場競賽將在高雄夢時代舉辦，是一個讓你和你的寵物一同參與的盛大活動。無論是你的兔子還是你的寵物鼠，都可以在這裡展現自己的才能，一展風采！比賽將包括各種有趣的趣味競賽項目，例如快樂小跑、障礙賽、食物競速等等。這些競賽既能展現你的寵物的活力和技巧，也能讓你們之間建立更加緊密的關係。",
+    },
+    {
+      id: 4,
+      year: 2024,
+      date: "02.20",
+      type: "店家",
+      imgPath: newsStore_1,
+      title: "另類寵物主題餐廳～10間不可錯過的特寵主題餐廳",
+      content:
+        "這篇收錄的不是一般的貓咪或狗狗寵物餐廳，而是以狐獴、巨嘴鳥、兔子、守宮、球蟒、鸚鵡、鬆獅蜥等為主題的另類特寵主題餐廳。這份精選介紹了10間獨特的寵物主題餐廳，快來親密互動，與這些可愛的萌寵們共度美好時光吧。",
+    },
+    {
+      id: 5,
+      year: 2024,
+      date: "02.19",
+      type: "店家",
+      imgPath: newsStore_2,
+      title: "動物嘉年華餐廳，天竺鼠、貓咪、兔子同樂的歡樂饗宴！",
+      content:
+        "這裡是兔子和天竺鼠們的冒險樂園。牠們在這裡玩耍、跳躍，總是充滿好奇心的樣子。您可以和兔子們互動，一同參與這場充滿歡樂的冒險，當然最吸引人的還是美味的饗宴。餐廳提供各種豐富的料理，滿足您的味蕾，同時，您可以在歡樂的氛圍中享受美食。",
+    },
+    {
+      id: 6,
+      year: 2024,
+      date: "02.17",
+      type: "店家",
+      imgPath: newsStore_3,
+      title: "與神秘的爬蟲類共度下午茶時光～還可跟牠們互動哦～",
+      content:
+        "在這裡，與爬蟲們的互動是不可或缺的環節。餐廳提供了專業的爬蟲導覽員，他們會教你如何與這些生物進行親密接觸，讓你不僅能看到牠們，還能親手摸摸，甚至學習更多有趣的爬蟲知識。",
+    },
+  ];
+
+  // News State
+  const [currentNewsType, setCurrentNewsType] = useState("店家");
+
+  // News Info
+  const NewsInfo = ({ news, index }) => (
+    <li
+      data-aos="fade-in"
+      data-aos-duration="500"
+      className="mt-[56px] flex gap-5 md:gap-10"
+    >
+      {/*  <!-- News Date --> */}
+      <h4 className="hidden md:flex items-center ">
+        <div
+          className={`${
+            index % 2 ? "bg-yellow-light" : "bg-green-light"
+          } p-[20px] rounded-xl text-brown-dark font-bold text-center`}
+        >
+          {news.year}
+          <span className="block">{news.date}</span>
+        </div>
+      </h4>
+      {/*  <!-- News Content --> */}
+      <Link
+        to={currentNewsType === "店家" ? "/page/store-search" : "/page/event"}
+        className="group flex flex-col-reverse md:flex-row gap-8 items-center md:border-l-2 border-brown-normal md:pl-6"
+      >
+        <div className="tracking-wide px-4">
+          <h3 className="ellipsis-2 text-brown-dark font-bold text-[24px]">
+            {news.title}
+          </h3>
+          <p className="ellipsis-3 mt-[12px] text-brown-normal text-[20px] leading-8 text-justify">
+            {news.content}
+          </p>
+        </div>
+        {/*  <!-- News Photos --> */}
+        <figure className="overflow-hidden min-w-[224px] rounded-lg">
+          <img
+            className="group-hover:scale-125"
+            src={news.imgPath}
+            alt="news photo"
+          />
+        </figure>
+      </Link>
+    </li>
+  );
+  // Filter News
+  const filterNews = newsData.filter((item) => item.type === currentNewsType);
 
   return (
     <>
@@ -150,26 +205,34 @@ export default function HomePage() {
             <div className="flex items-end justify-between">
               {/*  <!-- News Title --> */}
               <div className="mx-auto md:mx-0">
-                <h2 className="tracking-widest text-brown-dark font-black text-[50px]">
+                <h2 className="tracking-widest text-brown-dark font-bold text-[50px]">
                   News
                   <br className="sm:hidden" />
-                  <span className="text-green-normal text-[40px] font-black md:ml-4">
+                  <span className="text-green-normal text-[40px] font-bold md:ml-4">
                     最新消息
                   </span>
                 </h2>
-                <div className="mt-[32px] text-[20px] font-bold">
-                  <a
-                    className="px-[22px] py-[7px] mr-6 rounded-full bg-brown-light text-brown-dark"
-                    href="#"
+                <div className="mt-7 text-[20px] font-bold">
+                  <button
+                    className={`${
+                      currentNewsType === "活動"
+                        ? "bg-green-normal text-white"
+                        : "bg-brown-light text-brown-dark"
+                    } px-[22px] py-[7px] mr-6 rounded-full`}
+                    onClick={() => setCurrentNewsType("活動")}
                   >
                     活 動
-                  </a>
-                  <a
-                    className="px-[22px] py-[7px] rounded-full bg-green-normal text-white"
-                    href="#"
+                  </button>
+                  <button
+                    className={`${
+                      currentNewsType === "店家"
+                        ? "bg-green-normal text-white"
+                        : "bg-brown-light text-brown-dark"
+                    } px-[22px] py-[7px] rounded-full`}
+                    onClick={() => setCurrentNewsType("店家")}
                   >
                     店 家
-                  </a>
+                  </button>
                 </div>
               </div>
               {/*  <!-- See More --> */}
@@ -190,113 +253,14 @@ export default function HomePage() {
             </div>
             {/*  <!-- News List --> */}
             <ul className="mt-[24px] py-[16px] border-t-2 border-brown-normal">
-              <li
-                data-aos="fade-in"
-                data-aos-duration="500"
-                className="mt-[56px] flex gap-5 md:gap-10"
-              >
-                {/*  <!-- News Date --> */}
-                <h4 className="hidden md:flex items-center ">
-                  <div className="p-[20px] rounded-xl bg-green-light text-brown-dark font-bold text-center">
-                    2024
-                    <span className="block">01.22</span>
-                  </div>
-                </h4>
-                {/*  <!-- News Content --> */}
-                <article className="flex flex-col-reverse md:flex-row gap-8 items-center md:border-l-2 border-brown-normal md:pl-6">
-                  <div className="tracking-wide px-4">
-                    <h3 className="ellipsis-2 text-brown-dark font-bold text-[24px]">
-                      全台最大的爬蟲博覽會，即將在花博登場！
-                    </h3>
-                    <p className="ellipsis-3 mt-[12px] text-brown-normal text-[20px] leading-8 text-justify">
-                      親愛的爬蟲愛好者們，準備好了嗎？今年全台最大的爬蟲博覽會即將在花博登場！這將是一場爬蟲世界的盛宴，將展示各種令人驚嘆的爬蟲類動物，以及相關的設備、用品和知識。無論你是對爬蟲類動物充滿好奇心，還是已經是一位經驗豐富的飼主，這個博覽會都將是你不容錯過的活動！
-                    </p>
-                  </div>
-                  {/*  <!-- News Photos --> */}
-                  <figure className="overflow-hidden min-w-[224px] rounded-lg">
-                    <a href="#">
-                      <img
-                        className="hover:scale-125"
-                        src={newsStore_1}
-                        alt="news photo"
-                      />
-                    </a>
-                  </figure>
-                </article>
-              </li>
-              <li
-                data-aos="fade-in"
-                data-aos-duration="500"
-                className="mt-[56px] flex gap-5 md:gap-10"
-              >
-                {/*  <!-- News Date --> */}
-                <h4 className="hidden md:flex items-center ">
-                  <div className="p-[20px] rounded-xl bg-yellow-normal text-brown-dark font-bold text-center">
-                    2024
-                    <span className="block">02.01</span>
-                  </div>
-                </h4>
-                {/*  <!-- News Content --> */}
-                <article className="flex flex-col-reverse md:flex-row gap-8 items-center md:border-l-2 border-brown-normal md:pl-6">
-                  <div className="tracking-wide px-4">
-                    <h3 className="ellipsis-2 text-brown-dark font-bold text-[24px]">
-                      零距離觸摸爬蟲類動物，大小朋友都可參加！親子活動推薦
-                    </h3>
-                    <p className="ellipsis-3 mt-[12px] text-brown-normal text-[20px] leading-8 text-justify">
-                      一般人對爬蟲類動物往往感到恐懼，並且會避之唯恐不及。但許多家長或許已經發現，小朋友卻對這些生物感到非常感興趣，並且毫不畏懼。事實上，有許多團體舉辦各種爬蟲類動物的探索活動，這些活動讓小朋友能夠從實際體驗中認識和了解這些冷門動物的生態習性，並超越了書本上的知識。
-                    </p>
-                  </div>
-                  {/*  <!-- News Photos --> */}
-                  <figure className="overflow-hidden min-w-[224px] rounded-lg">
-                    <a href="#">
-                      <img
-                        className="hover:scale-125"
-                        src={newsStore_2}
-                        alt="news photo"
-                      />
-                    </a>
-                  </figure>
-                </article>
-              </li>
-              <li
-                data-aos="fade-in"
-                data-aos-duration="500"
-                className="mt-[56px] flex gap-5 md:gap-10"
-              >
-                {/*  <!-- News Date --> */}
-                <h4 className="hidden md:flex items-center ">
-                  <div className="p-[20px] rounded-xl bg-green-light text-brown-dark font-bold text-center">
-                    2024
-                    <span className="block">02.13</span>
-                  </div>
-                </h4>
-                {/*  <!-- News Content --> */}
-                <article className="flex flex-col-reverse md:flex-row gap-8 items-center md:border-l-2 border-brown-normal md:pl-6">
-                  <div className="tracking-wide px-4">
-                    <h3 className="ellipsis-2 text-brown-dark font-bold text-[24px]">
-                      高雄夢時代 - 鼠兔趣味競賽，歡迎飼主報名參加！
-                    </h3>
-                    <p className="ellipsis-3 mt-[12px] text-brown-normal text-[20px] leading-8 text-justify">
-                      這場競賽將在高雄夢時代舉辦，是一個讓你和你的寵物一同參與的盛大活動。無論是你的兔子還是你的寵物鼠，都可以在這裡展現自己的才能，一展風采！比賽將包括各種有趣的趣味競賽項目，例如快樂小跑、障礙賽、食物競速等等。這些競賽既能展現你的寵物的活力和技巧，也能讓你們之間建立更加緊密的關係。
-                    </p>
-                  </div>
-                  {/*  <!-- News Photos --> */}
-                  <figure className="overflow-hidden min-w-[224px] rounded-lg">
-                    <a href="#">
-                      <img
-                        className="hover:scale-125"
-                        src={newsStore_3}
-                        alt="news photo"
-                      />
-                    </a>
-                  </figure>
-                </article>
-              </li>
+              {filterNews.map((news, index) => (
+                <NewsInfo news={news} index={index} key={news.id} />
+              ))}
             </ul>
           </article>
         </div>
         {/*  <!-- Hot Article --> */}
-        <div className="mt-56 py-32 bg-cover bg-no-repeat bg-center bg-[url('../assets/index/hot-bg.svg')]">
+        <div className="mt-56 py-32 bg-cover bg-no-repeat bg-center 2xl:bg-contain 2xl:bg-gray-normal bg-[url('../assets/index/hot-bg.svg')]">
           <article className="relative">
             {/*  <!-- Chameleon --> */}
             <img
@@ -307,7 +271,7 @@ export default function HomePage() {
               alt="chameleon"
             ></img>
             {/*  <!-- Hot Info Title --> */}
-            <div className="container mx-auto relative text-center font-black tracking-wide">
+            <div className="container mx-auto relative text-center font-bold tracking-wide">
               <h2 className="text-brown-dark text-[48px]">
                 Hot
                 <span className="text-cyan-dark text-[40px] tracking-widest md:ml-4">
@@ -373,7 +337,7 @@ export default function HomePage() {
                       {article.animal}
                       <span className="text-brown-normal font-normal">
                         <img
-                          className="inline-block ml-1 mb-[2px]"
+                          className="inline-block mx-1 mb-[2px]"
                           src={timeIcon}
                           alt="time"
                         />
@@ -406,15 +370,15 @@ export default function HomePage() {
         </div>
         {/*  <!-- Follow APP Info --> */}
         <div className="container mx-auto my-36 px-2 lg:px-0 flex gap-6 justify-evenly items-center flex-col lg:flex-row">
-          <figure data-aos="zoom-in" data-aos-duration="1000">
+          <figure data-aos="zoom-in" data-aos-duration="1500">
             <img src={appDemo} alt="phone" />
           </figure>
           <section
-            data-aos="fade-lfet"
-            data-aos-duration="1000"
+            data-aos="fade-in"
+            data-aos-duration="1500"
             className="text-center"
           >
-            <h2 className="text-[40px] text-green-normal tracking-widest font-black">
+            <h2 className="text-[40px] text-green-normal tracking-widest font-bold">
               APP瀏覽更方便
             </h2>
             <h3 className="mt-2 text-[26px] sm:text-[30px] text-brown-dark font-bold tracking-wider">
@@ -422,13 +386,13 @@ export default function HomePage() {
             </h3>
             <div className="mt-14">
               <a
-                href="#"
+                href="https://play.google.com"
                 className="max-w-[251px] mx-auto block hover:opacity-70"
               >
                 <img src={googlePlay} alt="google-play" />
               </a>
               <a
-                href="#"
+                href="https://www.apple.com/tw/app-store"
                 className="max-w-[251px] mx-auto mt-[12px] block hover:opacity-70"
               >
                 <img src={appStore} alt="app-store" />
